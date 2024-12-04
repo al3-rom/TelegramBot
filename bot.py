1f57b23973ef08267bee2ac5d3e2d2645df4ca1b
al3-rom <sasha.stankevich223@gmail.com>
2024-12-04 12:33:37 +0100

    Errors resolving

--- bot.py
+++ bot.py
@@ -1,15 +1,27 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Application, CommandHandler
+import os
 
-# Токен твоего бота
-TOKEN = "7696894088:AAFKfBmcsPQ5x8aEcTmNfce4q253sd6MFF0"
+# Получение токена из переменной окружения
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-def start(update, context):
-    update.message.reply_text("Привет! Приглашай друзей и участвуй в розыгрыше. Используй свою ссылку: https://t.me/AirRushBot?start=" + str(update.effective_user.id))
+# Функция обработки команды /start
+async def start(update: Update, context):
+    unique_link = f"https://t.me/AirRushBot?start={update.effective_user.id}"
+    await update.message.reply_text(
+        f"Привет! Приглашай друзей и участвуй в розыгрыше. Используй свою ссылку: {unique_link}"
+    )
 
-# Запуск бота
-updater = Updater(TOKEN, use_context=True)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-updater.start_polling()
-updater.idle()
+# Основной код
+def main():
+    # Создаём приложение
+    application = Application.builder().token(TOKEN).build()
+
+    # Регистрируем обработчики
+    application.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
